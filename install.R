5dce78761fd8d8266604c066cb3c9f19db543fde
Juliette Hayer <juliette.hayer@gmail.com>
2018-11-30 10:45:55 +0100

    added tidyverse install

--- install.R
+++ install.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 repo='https://ftp.acc.umu.se/mirror/CRAN/'
 install.packages("RMySQL", repos=repo)
+install.packages("tidyverse", repos=repo)
 install.packages("BiocManager", repos=repo)
 BiocManager::install("GenomicFeatures")
 BiocManager::install("DESeq2")
